c973f1baa9074ba22439d6dc25b0584e4cb91eb4
Jorge Castro-Gallardo <jcastro.inf@gmail.com>
2022-02-27 17:37:34 +0100

    Initial commit
    Signed-off-by: Jorge Castro-Gallardo <jcastro.inf@gmail.com>

---
+++ dropstar-api-db/postgres_ssl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+sudo rm server.*
+rm privkey.pem
+
+openssl req -new -text -passout pass:abcd -subj /CN=localhost -out server.req -keyout privkey.pem
+openssl rsa -in privkey.pem -passin pass:abcd -out server.key
+openssl req -x509 -in server.req -text -key server.key -out server.crt
+chmod 600 server.key
+test $(uname -s) = Linux && sudo chown 70 server.key
---
+++ start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+source .env
+
+pushd dropstar-api-db
+    ./postgres_ssl.sh
+popd
+
+
+
+POSTGRES_URL="postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
+
+if [ ! -d "dropstar-api" ]; then
+    git clone git@github.com:etinxdev/dropstar-api.git
+fi
+
+pushd dropstar-api
+    git checkout develop
+    git pull
+    npm install
+popd
+
+pushd dropstar-api/src/database
+    echo "TEST_URL=${POSTGRES_URL}"      > .env
+    echo "DATABASE_URL=${POSTGRES_URL}" >> .env
+    npx sequelize db:migrate
+popd
